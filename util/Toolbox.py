83e5690a9a43ffcaa0d69814a85ad46512c792b8
Ken <jjian03@syr.edu>
2020-07-11 20:01:09 -0400

    Add keyword combination

--- util/Toolbox.py
+++ util/Toolbox.py
@@ -1,7 +1,11 @@
 import hashlib
 import json
+import pathlib
 
+import pandas as pd
 from ibranch.scraping_scheduler.domain.System import CONSTANT as BaseConstant
+from jproperties import Properties
+from singleton_decorator import singleton
 
 
 class CONSTANT(BaseConstant):
@@ -23,3 +27,29 @@ class MD5Encoder:
     def encode_json(json_obj, charset='utf-8'):
         json_str = json.dumps(json_obj, sort_keys=True, indent=0)
         return hashlib.md5(json_str.encode(charset)).hexdigest()
+
+
+class ParameterGenerator:
+    @staticmethod
+    def create_combination_matrix(params: dict) -> pd.DataFrame:
+        combination_list = pd.DataFrame({'dummy': [1]})
+        for key, values in params.items():
+            combination_list = pd.merge(combination_list, pd.DataFrame({key: values, 'dummy': [1] * len(values)}))
+        combination_list.drop('dummy', axis=1, inplace=True)
+        return combination_list
+
+
+@singleton
+class Keyword:
+    def __init__(self):
+        root_dir = pathlib.Path(__file__).parent.parent.absolute()
+        print(pathlib.Path(__file__).parent.parent.absolute())
+        with open(f'{root_dir}/resource/property/keyword.properties', 'rb') as config_file:
+            configs = Properties()
+            configs.load(config_file)
+
+            params = {k: v.data.lower().split(',') for k, v in configs.items()}
+            self._keyword_matrix = ParameterGenerator.create_combination_matrix(params)
+
+    def get_combination(self) -> pd.DataFrame:
+        return self._keyword_matrix
